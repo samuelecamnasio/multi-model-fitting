c5f209eb62360cd866f7b16b0e25b76f6fb4686f
Andrea Bonatti <andrea2.bonatti@mail.polimi.it>
2021-04-28 18:49:35 +0200

    added comments

--- LineFitter.py
+++ LineFitter.py
@@ -3,6 +3,8 @@ from numpy import *
 from math import sqrt
 
 
+# Fits the input points to a line,
+# returns the m and b parameters of a line
 def best_fit_slope_and_intercept(xs, ys):
     m = (((mean(xs) * mean(ys)) - mean(xs * ys)) /
          ((mean(xs) * mean(xs)) - mean(xs * xs)))
@@ -10,13 +12,10 @@ def best_fit_slope_and_intercept(xs, ys):
     return m, b
 
 
-def compute_error_for_lines(cluster_points, m, b):
-    # our noisy points
-    distance = abs(b + m * cluster_points[:, 0] - cluster_points[:, 1]) / sqrt(1 + m ** 2)
-    return distance, stdev(distance)
-
-
+# Returns the vector of the distances (between the points of the cluster
+# and the fitted line) and the standard deviation of the residuals
 def fit_on_fly_lines(cluster_points):
     m, b = best_fit_slope_and_intercept(cluster_points[:, 0], cluster_points[:, 1])
-    return compute_error_for_lines(cluster_points, m, b)
+    distance = abs(b + m * cluster_points[:, 0] - cluster_points[:, 1]) / sqrt(1 + m ** 2)
+    return distance, stdev(distance)
 
