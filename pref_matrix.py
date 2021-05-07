ec8539cfecaf765845f5c4bf13c030c678fb614b
samuc97 <samu-camnasio97@hotmail.com>
2021-05-07 14:35:44 +0200

    added function to be called on GRIC

--- pref_matrix.py
+++ pref_matrix.py
@@ -5,12 +5,65 @@ import numpy as np
 from Cluster import Cluster
 from LineFitter import fit_on_fly_lines
 from CircleFitter import fit_on_fly_circles
+from math import sqrt
 
 
 def distance_from_line(p1, p2,
                        p3):  # calculates the normal distance between a point p3 and a line passing through p1 and p2
     return (np.linalg.norm(np.cross(p2 - p1, p1 - p3)) / np.linalg.norm(p2 - p1)) == 0
 
+# Function to find the circle on
+# which the given three points lie
+def find_circle(x1, y1, x2, y2, x3, y3) :
+    x12 = x1 - x2
+    x13 = x1 - x3
+
+    y12 = y1 - y2
+    y13 = y1 - y3
+
+    y31 = y3 - y1
+    y21 = y2 - y1
+
+    x31 = x3 - x1
+    x21 = x2 - x1
+
+    # x1^2 - x3^2
+    sx13 = pow(x1, 2) - pow(x3, 2)
+
+    # y1^2 - y3^2
+    sy13 = pow(y1, 2) - pow(y3, 2)
+
+    sx21 = pow(x2, 2) - pow(x1, 2)
+    sy21 = pow(y2, 2) - pow(y1, 2)
+
+    f = (((sx13) * (x12) + (sy13) *
+          (x12) + (sx21) * (x13) +
+          (sy21) * (x13)) // (2 *
+          ((y31) * (x12) - (y21) * (x13))));
+
+    g = (((sx13) * (y12) + (sy13) * (y12) +
+          (sx21) * (y13) + (sy21) * (y13)) //
+          (2 * ((x31) * (y12) - (x21) * (y13))));
+
+    c = (-pow(x1, 2) - pow(y1, 2) -
+         2 * g * x1 - 2 * f * y1);
+
+    # eqn of circle be x^2 + y^2 + 2*g*x + 2*f*y + c = 0
+    # where centre is (h = -g, k = -f) and
+    # radius r as r^2 = h^2 + k^2 - c
+    h = -g;
+    k = -f;
+    sqr_of_r = h * h + k * k - c;
+
+    # r is the radius
+    r = round(sqrt(sqr_of_r), 5);
+
+    return h,k,r
+
+def distance_from_circ(p1, p2, p3,
+                       p4):  # calculates the normal distance between a point p4 and a circle passing through p1, p2 and p3
+    h,k,r = find_circle(p1[0], p1[1], p2[0], p2[1], p3[0], p3[1]) #find centre and radius
+    return abs(sqrt(pow(p4[0]-h,2) + pow(p4[1]-k,2)) - r)
 
 def get_preference_matrix(points):
     pref_mat = np.zeros((150, 15))
